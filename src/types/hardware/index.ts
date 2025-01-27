1baf3b66f1d6f04861eb931bdf369d760c46ecaf
Shapy-Sees <austinfissure11@hotmail.com>
2025-01-27 15:33:42 -0800

    Export HardwareAudioConfig and HardwareDAHDIConfig types for improved clarity in hardware type definitions.

--- src/types/hardware/index.ts
+++ src/types/hardware/index.ts
@@ -4,3 +4,5 @@ export * from './dahdi';
 export * from './audio';
 export * from './fxs';
 export * from '../hardware-config';
+export { AudioConfig as HardwareAudioConfig } from './audio';
+export { DAHDIConfig as HardwareDAHDIConfig } from './dahdi';
